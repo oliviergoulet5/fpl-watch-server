81a0d667737c90858219b51eb103c53150508888
Olivier Goulet <oliviergoulet5@gmail.com>
2021-05-03 15:27:31 -0400

    Added to resolver list

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,9 +1,11 @@
 import AccountResolver from './accountResolver/accountResolver';
 import PlayerResolver from './playerResolver/playerResolver';
 import FixtureResolver from './fixtureResolver/fixtureResolver';
+import { ClubResolver } from './clubResolver/clubResolver';
 
 export const resolvers: [Function, ...Array<Function>] = [ 
     AccountResolver, 
     PlayerResolver,
-    FixtureResolver 
+    ClubResolver,
+    FixtureResolver
 ];
